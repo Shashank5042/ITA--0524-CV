4645872ede7c3b10e7ab4641f62d2dcefd937e8e
Shashank5042 <130438579+Shashank5042@users.noreply.github.com>
2024-09-20 23:34:56 +0530

    Delete color (5).py

--- color (5).py
+++
@@ -1,8 +0,0 @@
-import cv2
-img = cv2.imread("/Users/chrisnikky/Downloads/helo.jpeg", cv2.IMREAD_COLOR)
-if img is None:
-    print("Error: Could not load image.")
-else:
-    cv2.imshow("image", img) 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
